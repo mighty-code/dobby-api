963273e8b97fe46401dc2c5e455bdddd02d8d658
Oliver Kaufmann <okaufmann91@gmail.com>
2020-01-10 18:16:43 +0100

    fix onboarding

--- app/Http/Controllers/OnboardingController.php
+++ app/Http/Controllers/OnboardingController.php
@@ -5,27 +5,25 @@ namespace App\Http\Controllers;
 use App\Events\UpdateNextConnection;
 use App\Http\Requests\StoreConnectionRequest;
 use App\User;
+use Auth;
+use Grimzy\LaravelMysqlSpatial\Types\Point;
 use Illuminate\Http\Request;
 
 class OnboardingController extends Controller
 {
-    /**
-     *
-     * @param StoreConnectionRequest $request
-     *
-     * @return \Illuminate\Http\RedirectResponse
-     */
     public function __invoke(StoreConnectionRequest $request)
     {
-        $connection = auth()->user()->connections()->create([
-            'from'            => $request->from['name'],
-            'to'              => $request->to['name'],
-            'via'             => $request->via['name'],
-            'station_id'      => $request->from['stationId'],
-            'destination_id'  => $request->to['stationId'],
-            'via_id'          => $request->via['stationId'],
+        $connection = Auth::user()->connections()->create([
+            'from' => $request->input('from.name'),
+            'station_id' => $request->input('from.stationId'),
+            'from_location' => new Point($request->input('from.latitude'), $request->input('from.longitude')),
+
+            'to' => $request->input('to.name'),
+            'destination_id' => $request->input('to.stationId'),
+            'to_location' =>  new Point($request->input('to.latitude'), $request->input('to.longitude')),
+
             'time_to_station' => $request->time_to_station,
-            'selected'        => true
+            'selected' => true,
         ]);
 
         event(new UpdateNextConnection($connection->id));
@@ -33,7 +31,6 @@ class OnboardingController extends Controller
         $user = User::find(auth()->user()->id);
         $user->first_login = false;
         $user->save();
-
 
         return $connection;
     }
