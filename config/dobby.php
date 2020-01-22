de6a2668414aab8b726aeb3f22c541892f7247d5
Oliver Kaufmann <okaufmann91@gmail.com>
2020-01-22 21:46:50 +0100

    add chrome extension url

--- config/dobby.php
+++ config/dobby.php
@@ -2,4 +2,5 @@
 
 return [
     'tag-manager-id' => env('GOOGLE_TAG_MANAGER_ID'),
+    'chrome-extension-url' => env('CHROME_EXTENSION_URL'),
 ];
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -12,6 +12,11 @@
                             <span class="text-muted"><a href="{{ route('manage') }}" class="link-unstyled"><i
                                             class="fa fa-code-branch"></i> Connections</a></span>
                         </div>
+                        @if($extensionUrl = config('dobby.chrome-extension-url'))
+                        <div class="col-12">
+                            <span class="text-muted"><a href="{{$extensionUrl}}" target="_blank" class="link-unstyled"><i class="fab fa-chrome"></i> Chrome Extension</a></span>
+                        </div>
+                        @endif
                         <div class="col-12">
                             <span class="text-muted"><a href="{{ route('api.manage') }}" class="link-unstyled"><i
                                             class="fa fa-puzzle-piece"></i> API</a></span>
@@ -19,13 +24,11 @@
                     @endif
                     <div class="col-12">
                         <span class="text-muted">
-    
                             <a class="link-unstyled" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
                             <i class="fa fa-sign-out-alt"></i> Sign out</span>
                         </a>
-    
                     </div>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
