5f695ddcf0bac2b34e8a2fc0899b20c2a070f302
Oliver Kaufmann <okaufmann91@gmail.com>
2020-01-24 01:16:22 +0100

    setup broadcasting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
+                'useTLS' => true,
+                'host' => env('PUSHER_HOST', '127.0.0.1'),
+                'port' => env('PUSHER_PORT', 6001),
+                'scheme' => env('PUSHER_ENCRYPTED', true) ? 'https' : 'http',
             ],
         ],
 
