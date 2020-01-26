70794bdb3ae6d7e1c91bb83eb6eaaf336e70c564
Oliver Kaufmann <okaufmann91@gmail.com>
2020-01-26 23:31:49 +0100

    refactor user entpoint to resource

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,7 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    Route::get('/user', UserProfileController::class);
 
     Route::get('connection/next', [ConnectionController::class, 'nextConnection']);
 });
